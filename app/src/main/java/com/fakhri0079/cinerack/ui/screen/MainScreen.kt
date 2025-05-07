7c562d651516868845a2a156f201730e2152341a
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-05-07 21:10:33 +0700

    empty state declaration

--- app/src/main/java/com/fakhri0079/cinerack/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fakhri0079/cinerack/ui/screen/MainScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -49,16 +50,26 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
-
-    LazyColumn(
-        modifier = modifier.fillMaxSize(),
-    ) {
-        items(data) {
-            ListItem(film = it)
-            HorizontalDivider()
+    if (data.isEmpty()){
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.empty_list))
         }
+    }else{
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+        ) {
+            items(data) {
+                ListItem(film = it)
+                HorizontalDivider()
+            }
 
+        }
     }
+
 }
 
 @Composable
